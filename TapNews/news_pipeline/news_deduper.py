7de6eda16674fe85665743e79c183e98d28017cc
Nathalie <hechenchen9@gmail.com>
2017-10-10 14:35:33 -0400

    correct table name

--- TapNews/news_pipeline/news_deduper.py
+++ TapNews/news_pipeline/news_deduper.py
@@ -37,8 +37,8 @@ cloudAMQP_client = CloudAMQPClient(config['news_deduper']['DEDUPE_NEWS_TASK_QUEU
                                    config['news_deduper']['DEDUPE_NEWS_TASK_QUEUE_NAME'])
 
 def handle_message(msg):
-    if msg is None or not isinstance(msg, dict):
-        return
+    #if msg is None or not isinstance(msg, dict):
+        #return
 
     task = msg
     text = task['text']
@@ -51,7 +51,7 @@ def handle_message(msg):
     published_at_day_end = published_at_day_begin + datetime.timedelta(days=1)
     print 'hello'
     db = mongo_client.get_db()
-    same_day_news_list = list(db['NEWS_TABLE_NAME'].find(
+    same_day_news_list = list(db[NEWS_TABLE_NAME].find(
         {'publishedAt': {'$gte': published_at_day_begin,
                          '$lt': published_at_day_end}}))
     print 'how are you'
@@ -79,14 +79,14 @@ def handle_message(msg):
         topic = news_topic_modeling_service_client.classify(title)
         task['class'] = topic
     print 'what is wrong'
-    db['NEWS_TABLE_NAME'].replace_one({'digest': task['digest']}, task, upsert=True)
+    db[NEWS_TABLE_NAME].replace_one({'digest': task['digest']}, task, upsert=True)
 
 
 while True:
 
     if cloudAMQP_client is not None:
         msg = cloudAMQP_client.getMessage()
-        if msg is None:
+        if msg is not None:
            #Parse and process the task
            try:
                handle_message(msg)
